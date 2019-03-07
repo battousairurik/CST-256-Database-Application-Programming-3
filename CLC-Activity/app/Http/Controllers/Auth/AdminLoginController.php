e4d51c14e2e358d8c186eca39e81b49ac32c711e
Michael Weaver <georgemclinly@yahoo.com>
2019-03-07 18:53:30 -0500

    Admin Login Functionality 100%

--- CLC-Activity/app/Http/Controllers/Auth/AdminLoginController.php
+++ CLC-Activity/app/Http/Controllers/Auth/AdminLoginController.php
@@ -4,13 +4,31 @@ namespace App\Http\Controllers\Auth;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 
 class AdminLoginController extends Controller
 {
+    public function __construct(){
+        $this->middleware('guest:admin');
+    }
+    
     public function showLoginForm(){
         return view('auth.admin-login');
     }
-    public function login(){
-        return true;
+    public function login(Request $request){
+        //validate the form data
+        $this->validate($request, [
+            'email' => 'required|email',
+            'password' => 'required|min:6'
+        ]);
+        
+        //attempt to login the user
+        if(Auth::guard('admin')->attempt(['email' => $request->email, 'password' => $request->password], $request->remember)){
+            //if successful, redirect to intended location
+            return redirect()->intended(route('admin.dashboard'));
+        }
+         
+        //if unsucessful, redirect to login with form data
+        return redirect()->back()->withInput($request->only('email', 'remember'));
     }
 }
